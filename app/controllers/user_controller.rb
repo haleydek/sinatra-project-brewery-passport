413e8246ddb77aa6f2f8b249c721022c86c8d7ae
haleydek <hcd09@live.com>
2019-07-10 12:44:51 -0500

    create login, signup, and breweries routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,7 +3,15 @@ class UserController < ApplicationController
         if is_logged_in?
             redirect '/breweries'
         else
-            erb :signup
+            erb :'/users/signup'
+        end
+    end
+
+    get '/login' do
+        if is_logged_in?
+            redirect '/breweries'
+        else
+            erb :'/users/login'
         end
     end
 
@@ -14,6 +22,17 @@ class UserController < ApplicationController
    #    add slug for username in route
     end
 
+    post '/login' do
+        user = User.find_by(username: params[:username])
 
+        if is_logged_in?
+            redirect '/breweries'
+        elsif user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect '/breweries'
+        else
+            redirect '/login'
+        end
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
     has_many :visits
     has_many :breweries, through: :visits
+
+    has_secure_password
 end
