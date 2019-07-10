7e3fe14682c5b55a0958498b8826c9b2f992985f
haleydek <hcd09@live.com>
2019-07-10 12:30:18 -0500

    create routes in user & brewery controllers

--- app/controllers/brewery_controller.rb
+++ app/controllers/brewery_controller.rb
@@ -1,3 +1,12 @@
 class BreweryController < ApplicationController
+    get '/breweries' do
+        if is_logged_in?
+            @user = current_user
+            @breweries = Brewery.all
+            erb :'/breweries/index'
+        else
+            redirect '/login'
+        end
+    end
 
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,19 @@
 class UserController < ApplicationController
+    get '/signup' do
+        if is_logged_in?
+            redirect '/breweries'
+        else
+            erb :signup
+        end
+    end
+
+    post '/signup' do
+        user = User.create(params[:id])
+        session[:user_id] = user.id
+        redirect '/'
+   #    add slug for username in route
+    end
+
+
 
 end
