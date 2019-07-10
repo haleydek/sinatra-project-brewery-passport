d937d821f0ccb9b55d010d0febce9d1674d2479a
haleydek <hcd09@live.com>
2019-07-10 11:30:57 -0500

    setup application controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "mn_brew"
+    end
+      
+    get '/' do
+        erb :home
+    end
+
+    helpers do
+        def is_logged_in?
+            !!session[:user_id]
+        end
+          
+        def current_user
+            @current_user ||= User.find(session[:user_id])
+        end
+    end
+
+end
