1671d2ee9b45bd18e0149f22a48dc26daf9bc22c
haleydek <hcd09@live.com>
2019-07-11 11:07:56 -0500

    validation for user email, username, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
     has_many :visits
     has_many :breweries, through: :visits
 
+    validates_presence_of :username, :email, :password
+    validates_uniqueness_of :username, :email { case_sensitive: false }
+
     has_secure_password
 end
