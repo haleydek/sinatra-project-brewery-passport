79ee47d260d2fb5ccf549a813ffea3638b5f52ab
haleydek <hcd09@live.com>
2019-07-11 14:07:33 -0500

    Create passport form and controller action, create passport show view

--- app/controllers/brewery_controller.rb
+++ app/controllers/brewery_controller.rb
@@ -1,7 +1,6 @@
 class BreweryController < ApplicationController
     get '/breweries' do
         if is_logged_in?
-            @user = current_user
             @breweries = Brewery.all
             erb :'/breweries/index'
         else
--- app/controllers/visit_controller.rb
+++ app/controllers/visit_controller.rb
@@ -1,7 +1,28 @@
 class VisitController < ApplicationController
 
     get '/create-passport' do
-        
+        @user = current_user
+        if is_logged_in?
+            erb :'/visits/new'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/passport/:user_id'
+        if is_logged_in? && current_user
+            @user = current_user
+            erb :'/visits/show'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/create-passport' do
+        params[:visit][:brewery_ids].each do |brewery_id|
+            Visit.where(user_id: current_user.id, brewery_id: brewery_id).first_or_create
+        end
+        redirect "/passport/#{current_user.id}"
     end
 
 end
