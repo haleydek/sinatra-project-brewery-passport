fab2d1670525a8c984ccc4615b17b4ef85eec72a
haleydek <hcd09@live.com>
2019-07-11 11:13:29 -0500

    revise uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
     has_many :breweries, through: :visits
 
     validates_presence_of :username, :email, :password
-    validates_uniqueness_of :username, :email { case_sensitive: false }
+    validates :username, uniqueness: { case_sensitive: false }
+    validates :email, uniqueness: { case_sensitive: false }
 
     has_secure_password
 end
