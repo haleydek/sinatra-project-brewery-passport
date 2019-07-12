253d16e06e7b8fde806064c0fa726a01239e43b1
haleydek <hcd09@live.com>
2019-07-12 16:54:24 -0500

    add validation messages to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@ class User < ActiveRecord::Base
     has_many :visits
     has_many :breweries, through: :visits
 
-    validates_presence_of :username, :email, :password
-    validates :username, uniqueness: { case_sensitive: false }
-    validates :email, uniqueness: { case_sensitive: false }
-    validates_format_of :email, :with => /@/
+    validates_presence_of :username, :email, :password, message: "All fields must be filled in to continue."
+    validates :username, uniqueness: { case_sensitive: false }, message: "Sorry, this username already exists! Please try a different username."
+    validates :email, uniqueness: { case_sensitive: false }, message: "This email already has an account. Please try logging in with this email or enter a different email."
+    validates_format_of :email, with: /@/, message: "Please enter a valid email."
 
     has_secure_password
 end
