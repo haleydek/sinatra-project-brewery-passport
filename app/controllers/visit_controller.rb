917ca7bfa53e128258beaa7ca38fd27033a4da64
haleydek <hcd09@live.com>
2019-07-12 11:36:49 -0500

    create visits edit controller action and edit view, add edit link to vists/show view

--- app/controllers/visit_controller.rb
+++ app/controllers/visit_controller.rb
@@ -1,28 +1,79 @@
 class VisitController < ApplicationController
 
     get '/create-passport' do
+        @user = current_user
         if is_logged_in?
-            @user = current_user
-            erb :'/visits/new'
+            if @user.breweries == []
+                erb :'/visits/new'
+            else
+                redirect "/passport/#{current_user.id}"
+            end
         else
             redirect '/login'
         end
     end
 
     get '/passport/:user_id' do
-        if is_logged_in? && current_user
-            @user = current_user
+        if is_logged_in?
+            @user = User.find_by(id: params[:user_id])
             erb :'/visits/show'
         else
             redirect '/login'
         end
     end
 
-    post '/create-passport' do
-        params[:visit][:brewery_ids].each do |brewery_id|
-            Visit.where(user_id: current_user.id, brewery_id: brewery_id).first_or_create
+    get '/passport/:user_id/edit' do
+        @user = User.find_by(id: params[:user_id])
+        if is_logged_in?
+            if current_user.id == params[:user_id].to_i
+                erb :'/visits/edit'
+            else
+                redirect '/breweries'
+            end
+        else
+            redirect '/login'
         end
-        redirect "/passport/#{current_user.id}"
     end
 
+    post '/create-passport' do
+        if is_logged_in?
+            params[:visit][:brewery_ids].each do |brewery_id|
+                Visit.where(user_id: current_user.id, brewery_id: brewery_id).first_or_create
+            end
+            redirect "/passport/#{current_user.id}"
+        else
+            redirect '/login'
+        end
+    end
+
+    patch '/passport/:user_id' do
+        if is_logged_in?
+            if current_user.id == params[:user_id].to_i
+                params[:visit][:brewery_ids].each do |brewery_id|
+                    Visit.where(user_id: current_user.id, brewery_id: brewery_id).first_or_create
+                end
+                redirect "/passport/#{current_user.id}"
+            else
+                redirect '/breweries'
+            end
+        else
+            redirect '/login'
+        end
+    end
+
+    delete '/passport/:user_id' do
+        if is_logged_in?
+            if current_user.id == params[:user_id].to_i
+                params[:visit][:brewery_ids].each do |brewery_id|
+                    visit = Visit.find_by(user_id: current_user.id, brewery_id: brewery_id)
+                    visit.delete
+                end
+                redirect "/passport/#{current_user.id}"
+            else
+                redirect '/breweries'
+            end
+        else
+            redirect '/login'
+        end
+    end
 end
