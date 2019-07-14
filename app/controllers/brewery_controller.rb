35de2ffaa193566a54105d815cb52ebb66a6f928
haleydek <hcd09@live.com>
2019-07-14 12:42:27 -0500

    add delete button to breweries show page and styled breweries show page

--- app/controllers/brewery_controller.rb
+++ app/controllers/brewery_controller.rb
@@ -19,16 +19,32 @@ class BreweryController < ApplicationController
     # change route to slug for brewery name
     end
 
-    post '/create-visit' do
-        brewery = Brewery.find_by(id: params[:visit][:brewery_id])
-        user = User.find_by(id: params[:visit][:user_id])
+    post '/breweries/:id' do
+        @brewery = Brewery.find_by(id: params[:visit][:brewery_id])
+        @user = User.find_by(id: params[:visit][:user_id])
         if is_logged_in?
-            if user.id == current_user.id
-                Visit.where(user_id: user.id, brewery_id: brewery.id).first_or_create
-                redirect "/passport/#{current_user.id}"
+            if @user.id == current_user.id
+                Visit.where(user_id: @user.id, brewery_id: @brewery.id).first_or_create
+                redirect "/passport/#{@user.id}"
             else
                 redirect "/breweries"
                 #flash message that they can only edit their own passport
+            end
+        else
+            redirect '/login'
+        end
+    end
+
+    delete '/breweries/:id' do
+        @brewery = Brewery.find_by(id: params[:visit][:brewery_id])
+        @user = User.find_by(id: params[:visit][:user_id])
+        if is_logged_in?
+            if @user.id == current_user.id
+                visit = Visit.find_by(user_id: @user.id, brewery_id: @brewery.id)
+                visit.delete
+                redirect "/passport/#{@user.id}"
+            else
+                redirect '/breweries'
             end
         else
             redirect '/login'
