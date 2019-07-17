8043cd01bd132035463a6b1260149f2f894b26db
haleydek <hcd09@live.com>
2019-07-17 12:01:20 -0500

    revise message for email validation of uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
     validates_presence_of :username, :email, :password, message: "All fields must be filled in to continue."
     validates :username, uniqueness: { case_sensitive: false, message: "Sorry, this username already exists! Please try a different username." }
-    validates :email, uniqueness: { case_sensitive: false, message: "This email already has an account. Please try logging in with this email or enter a different email." }
+    validates :email, uniqueness: { case_sensitive: false, message: "This email already has an account." }
     validates_format_of :email, with: /@/, message: "Please enter a valid email."
 
     has_secure_password
